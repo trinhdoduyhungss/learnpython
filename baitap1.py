da03bdb7ca903989d3f00bbffc8d9bbc7e432769
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-03-03 14:48:58 +0700

    add note

--- baitap1.py
+++ baitap1.py
@@ -1,8 +1,9 @@
-#các biến xác định hình
+#các biến trục oxy xác định hình
 ncanh = int(input("Nhập số cạnh:"))
 x = int(input("Nhập độ dài x:"));
 y = int(input("Nhập độ dài y:"));
 o = int(input("Nhập vị trí y:"));
+#d : là điểm giao giữa đường thẳng song song y giao với đường thẳng song song với x
 d = int(input("Nhập vị trí đường thẳng song song y giao với đường thẳng song song với x:"));
 #vẽ tam giác cân
 if o*2==x  and o!=0 and d*2==x and ncanh==3 :
