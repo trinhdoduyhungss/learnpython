9918e80f465179341faa72dd1bba3826927b85cc
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-03-12 08:46:03 +0700

    homework update

--- tuongtacfile.py
+++ tuongtacfile.py
@@ -1,12 +1,13 @@
 
 filename = "commands.txt"
-d = ''
-commands = {}
-component = {}
-component2 = {}
 key = ''
+""" #biến dành cho cách 1
+commands = dict()
+component = {}
+component2 ={}"""
+result = {}
 with open(filename, "r") as fh:
-    # cách 1:
+    """# cách 1:
     for line in fh:
         command = line.strip().split('\n')
         if '[' in command[0] and ']' in command[0]:
@@ -15,7 +16,6 @@ with open(filename, "r") as fh:
         if '=' in command[0]:
             command2, des = command[0].strip().split('=')
             s = des.strip().split('|')
-            #tối ưu hóa được các đặc component thì sẽ tối ưu hóa đc code
             component[command2] = s[0]
             commands[convertlineagain[0]] = component
 
@@ -33,28 +33,39 @@ with open(filename, "r") as fh:
                     commands[convertlineagain[0]] = component2
 
             break
+
     """
-    # cách 2 - chưa xong:
+#cách 2 - tối ưu nhất:
     for line in fh:
         command = line.strip().split('\n')
-        command2 = line.strip().split('=')
-        if '[' in line and ']' in line:
-            convertline = line.split('[')
+        if '[' in command[0] and ']' in command[0]:
+            convertline = command[0].split('[')
             convertlineagain = convertline[1].split(']')
             key = convertlineagain[0]
-        if '=' in command[0] :
-            if component2 == {}:
-                s = command2[1].strip().split('|')
-                component[command2[0]] = s[0]
-                commands[key] = component
-            elif component2 != {}:
-               ss = command2[1].strip().split('|')
-               print(ss)
+            result[convertlineagain[0]]=dict()
+        if '=' in command[0]:
+            command2, des = command[0].strip().split('=')
+            s = des.strip().split('|')
+            obj = {command2:s[0]}
+            result[key].update(obj)
+print(result)
+"""#cách 3:
+input_file = open("config.txt",'r')
+content = [i[:-1] for i in input_file.readlines()]
+result = dict()
+for idx, e in enumerate(content):
 
+    if "[" in e:
+        content[idx] = content[idx].replace('[', '')
+        content[idx] = content[idx].replace(']', '')
+        key = idx
+        result[content[idx]] = dict()
+    elif "=" in e:
+        content[idx] = e[:e.index('|')]
+        e = e[:e.index('|')]
+        obj = {e[:e.index('=')].strip():  e[e.index('=') + 1:].strip()}
+        if key < idx:
+            result[content[key]].update(obj)
 
-        if command[0] == '':
-            component2 = component
-            #print(component2)
-            #break
-    """
-print(commands)
+print(result)"""
+
