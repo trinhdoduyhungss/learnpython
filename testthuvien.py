8612887b99ad5d433330225ad7f5282f5eaa5001
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-03-16 08:08:25 +0700

    update

---
+++ testthuvien.py
@@ -0,0 +1,10 @@
+import thuvien
+
+# Bai tap 1:
+str_path = 'config.txt'
+print(thuvien.read(str_path))
+print('Number of Line:',thuvien.get_number_of_line(str_path))
+print('number of Character:',thuvien.get_number_of_character(str_path))
+print('Number of Word',thuvien.get_number_of_word(str_path))
+# bai1.write('Hi there!')
+# bai1.erase(str_path)
