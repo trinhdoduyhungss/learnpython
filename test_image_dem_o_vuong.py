04a2d7a38523addc6a6b9041107bd3ddc9de25f4
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-04-07 16:32:17 +0700

    update_fix_dem_o_vuong_trang_va_do

--- test_image_dem_o_vuong.py
+++ test_image_dem_o_vuong.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
-img = cv2.imread("ovuong.png")
+img = cv2.imread("ovuong2.png")
 img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 img2 = cv2.resize(img2, (20, 20))
 x2= np.array(img2)
 #print(x2)
 count_do1 = 0
 count_do2 = 0
-count_trang = 0
 for i in range(len(x2)):
    for y in range(len(x2[i])):
        if (x2.item((i,y)) == 90):
@@ -16,8 +15,9 @@ for i in range(len(x2)):
        if (x2.item((i,y)) == 91):
            count_do2 += 1
            # print('x2.item(('+str(i)+','+str(y)+')) '+str(count))
-       elif (x2.item((i,y)) == 248):
-           count_trang += 1
-
-print('Số ô màu đỏ : ',(count_do1+count_do1/4+count_do2)/4 +1) #2 dòng một ô, mỗi ô = 4 lần 90
-print('số ô mày trắng :', (count_trang+count_trang/4)/4 - 2)
+so_o_do = (count_do1+count_do1/4+count_do2)/4 + 1
+print('Số ô màu đỏ : ', so_o_do) #2 dòng một ô, mỗi ô = 4 lần 90
+if (count_do2 >= 1):
+    print('số ô mày trắng :', (len(x2) * len(x2)) / 4 - so_o_do - count_do2/4 +2)
+else:
+    print('số ô mày trắng :', (len(x2) * len(x2)) / 4 - so_o_do + 1)
