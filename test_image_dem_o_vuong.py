a6c15e316aa900ca31a0186a4737b35c84ae9ba5
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-04-07 15:57:33 +0700

    update

--- test_image_dem_o_vuong.py
+++ test_image_dem_o_vuong.py
@@ -1,3 +1,16 @@
 import cv2
 import numpy as np
-img = cv2.imread("digits.png")
+img = cv2.imread("ovuong.png")
+img2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img2 = cv2.resize(img2, (20, 20))
+x2= np.array(img2)
+#print(x2)
+count = 0
+for i in range(len(x2)):
+   for y in range(len(x2[i])):
+       if (x2.item((i,y)) == 90):
+           count += 1
+           #print('x2.item(('+str(i)+','+str(y)+')) '+str(count))
+
+
+print((count+count/4)/4 +1) #2 dòng một ô, mỗi ô = 4 lần 90
