1b3a18ad986b68e288b5f09231a1e4c32dc4e2e9
Trinh Hung <trinhdoduyhungss@gmail.com>
2019-04-29 20:59:04 +0700

    update

--- matran/test_matran_with_convolution.py
+++ matran/test_matran_with_convolution.py
@@ -1,17 +1,14 @@
 import numpy as np
+import random
 
 mat = np.random.randint(0, 255, (6, 6))
 kernel = np.random.randint(-2, 2, (3, 3))
 
-def conv_transform(kernel):
-    kernel_copy = kernel.copy()
-    for i in range(kernel.shape[0]):
-        for j in range(kernel.shape[1]):
-            kernel_copy[i][j] = kernel[kernel.shape[0]-i-1][kernel.shape[1]-j-1]
-    return kernel_copy
 
-def conv(matran,kernel):
-    kernel = conv_transform(kernel)
+def conv(matran, kernel):
+    kernel = kernel
+    print("kernel:\n")
+    print(kernel, "\n")
 
     matran_h = matran.shape[0]
     matran_w = matran.shape[1]
@@ -19,19 +16,21 @@ def conv(matran,kernel):
     kernel_h = kernel.shape[0]
     kernel_w = kernel.shape[1]
 
-    h = kernel_h//2
-    w = kernel_w//2
+    h = kernel_h // 2
+    w = kernel_w // 2
 
     matran_conv = np.zeros(matran.shape)
 
-    for i in range(h, matran_h-h):
-        for j in range(w, matran_w-w):
+    for i in range(h, matran_h - h):
+        for j in range(w, matran_w - w):
             sum = 0
             for m in range(kernel_h):
                 for n in range(kernel_w):
-                    sum += kernel[m][n]*matran[i-h-m][j-w-n]
-                matran_conv[i][j] = sum
-    print('matran_conv',matran_conv)
+                    sum += kernel[m][n] * matran[i - h - m][j - w - n]
+                    matran_conv[i][j] = sum
+    print('matran_conv:\n')
+    print(matran_conv, "\n")
     return matran_conv
 
-conv(mat,kernel)
+
+conv(mat, kernel)
---
+++ matran/test_matran_with_convolution_inverse_kernel.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+mat = np.random.randint(0, 255, (6, 6))
+kernel = np.random.randint(-2, 2, (3, 3))
+
+# đảo ma trận
+def conv_transform(kernel):
+    kernel_copy = kernel.copy()
+    for i in range(kernel.shape[0]):
+        for j in range(kernel.shape[1]):
+            kernel_copy[i][j] = kernel[kernel.shape[0]-i-1][kernel.shape[1]-j-1]
+    return kernel_copy
+
+def conv(matran,kernel):
+    kernel = conv_transform(kernel)
+
+    matran_h = matran.shape[0]
+    matran_w = matran.shape[1]
+
+    kernel_h = kernel.shape[0]
+    kernel_w = kernel.shape[1]
+
+    h = kernel_h//2
+    w = kernel_w//2
+
+    matran_conv = np.zeros(matran.shape)
+
+    for i in range(h, matran_h-h):
+        for j in range(w, matran_w-w):
+            sum = 0
+            for m in range(kernel_h):
+                for n in range(kernel_w):
+                    sum += kernel[m][n]*matran[i-h-m][j-w-n]
+                matran_conv[i][j] = sum
+    print('matran_conv',matran_conv)
+    return matran_conv
+
+conv(mat,kernel)
